3da34c7b6dfaab45b397e9a40aadb6c1792e3a9d
Guilherme Melo <guilhermeromelo@gmail.com>
2021-02-10 02:13:21 -0300

    feat: FirebaseFirestore config

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 //import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:vigilancia_app/views/doormanAndGuard/doormanAndGuardRegistration.dart';
 import 'package:vigilancia_app/views/menu/menu.dart';
@@ -7,8 +9,10 @@ import 'package:vigilancia_app/views/users/userRegistration.dart';
 import 'package:vigilancia_app/views/workplace/workplaceRegistration.dart';
 
 void main() async {
-  //WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  //FirebaseFirestore.instance.collection("teste").doc().set({"deuCerto":true});
 
   runApp(MaterialApp(
     initialRoute: 'menu',
